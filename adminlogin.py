95380b14031945cf7fd3ec9ca59d2c728d561e40
aSkryabina <aSkryabina@users.noreply.github.com>
2018-07-18 16:41:16 +0300

    test_login

--- adminlogin.py
+++ adminlogin.py
@@ -9,7 +9,7 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
-def test_example(driver):
+def test_login(driver):
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
