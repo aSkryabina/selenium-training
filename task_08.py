dcb66f9565df048ed003a520740660aebb3d1d04
aSkryabina <aSkryabina@users.noreply.github.com>
2018-07-26 18:28:16 +0300

    Поиск стикеров (Задание 8)

--- task_08.py
+++ task_08.py
@@ -12,9 +12,17 @@ def driver(request):
 def test_stickers(driver):
     driver.get("http://localhost/litecart/")
     ducks = driver.find_elements_by_xpath("//div[contains(@class,'image-wrapper')]")
+    count = 0
     for duck in ducks:
         stickers = duck.find_elements_by_xpath(".//div[contains(@class,'sticker')]")
-        len(stickers) == 1
+        if not(len(stickers) == 1):
+            print("Sticker is missing")
+        else:
+            count = count + 1
+    if len(ducks) == count:
+        print("All items have a sticker")
+
+
 
 
 
