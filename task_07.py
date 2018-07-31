008666003d5a4c2beae5bfdcb639aebb60680918
aSkryabina <aSkryabina@users.noreply.github.com>
2018-07-31 16:43:06 +0300

    Добавлены комментарии в задание 7

--- task_07.py
+++ task_07.py
@@ -11,18 +11,22 @@ def driver(request):
 
 
 def test_menu(driver):
+    #авторизация в админке
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("remember_me").click()
     driver.find_element_by_name("login").click()
 
+    #поиск всех элементов основного меню
     menu = driver.find_elements_by_xpath("//ul[@id='box-apps-menu']//li[@id='app-']")
+    #проходим по всем элементам меню, кликаем на каждый пункт и проверяем, что на загруженной странице есть заголовок (h1)
     for i in range(len(menu)):
         menu[i].click()
         h1 = driver.find_elements_by_xpath("//h1")
         if len(h1) == 0:
             print("Page hasn't elements h1")
+        #проверяем, есть ли вложенные элементы у текущего пункта меню
         second = driver.find_elements_by_xpath("//li[@id='app-' and contains(@class, 'selected')]//li")
         if len(second) > 0:
             for k in range(len(second)):
@@ -30,5 +34,7 @@ def test_menu(driver):
                 h1 = driver.find_elements_by_xpath("//h1")
                 if len(h1) == 0:
                     print("Page hasn't elements h1")
+                #переопределаем список вложенных элементов
                 second = driver.find_elements_by_xpath("//li[@id='app-' and contains(@class, 'selected')]//li")
+        #переопределаем список элементов основного меню
         menu = driver.find_elements_by_xpath("//ul[@id='box-apps-menu']//li[@id='app-']")
