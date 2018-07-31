7090bf939b6d1dd04409fab169db45b5d9c94b22
aSkryabina <aSkryabina@users.noreply.github.com>
2018-07-31 18:03:45 +0300

    Проверка сортировки (Задание 9: часть 1 и 2)

--- task_09.py
+++ task_09.py
@@ -12,12 +12,14 @@ def driver(request):
 
 
 def test_countries(driver):
-    #авторизация в админке
+    '''авторизация в админке'''
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("remember_me").click()
     driver.find_element_by_name("login").click()
+
+    '''проверка сортировки в разделе "Countries"'''
 
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
     #поиск всех строк таблицы Countries, создание пустого списка и вычисление количества строк
@@ -41,7 +43,8 @@ def test_countries(driver):
                 zone = line.find_element_by_xpath("./td[3]").text
                 zones.append(zone)
             # сравнение исходного списка зон с отсортированным списком
-
+            table = driver.find_element_by_css_selector("table")
+            zones_url = table.get_attribute("baseURI")
             if sorted(zones) == zones:
                 print('The list of zones on page ' + zones_url + ' is sorted')
             else:
@@ -51,7 +54,40 @@ def test_countries(driver):
             rows = driver.find_elements_by_css_selector("table.dataTable tr.row")
         countries.append(country) #добавление страны в список
     #сравнение исходного списка стран с отсортированным списком
+    table = driver.find_element_by_css_selector("table")
+    countries_url = table.get_attribute("baseURI")
     if sorted(countries) == countries:
         print('The list of countries on page ' + countries_url + ' is sorted')
     else:
         print('The list of countries on page ' + countries_url + ' is not sorted')
+
+
+    '''проверка сортировки в разделе "Geo Zones"'''
+
+    driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+    # поиск всех строк таблицы Geo Zones, создание пустого списка и вычисление количества строк
+    rows = driver.find_elements_by_css_selector("table.dataTable tr.row")
+    countries = []
+    count = len(rows)
+    # прохождение по циклу с распарсиванием строки, записью стран в новый список и проверкой количества зон
+    for i in range(count):
+        row = rows[i]
+        row.find_element_by_xpath("./td[3]/a").click()
+        lines = driver.find_elements_by_css_selector("table#table-zones tr")
+        counter = len(lines)
+        zones = []
+        # прохождение по циклу (исключая первый и последний элемент) с распарсиванием строки и записью выбранных в выпадающем списке зон в новый список
+        for j in range(1, counter - 1):
+            line = lines[j]
+            zone = line.find_element_by_xpath("./td[3]//option[@selected]").text
+            zones.append(zone)
+        # сравнение исходного списка зон с отсортированным списком
+        table = driver.find_element_by_css_selector("table")
+        zones_url = table.get_attribute("baseURI")
+        if sorted(zones) == zones:
+            print('The list of zones on page ' + zones_url + ' is sorted')
+        else:
+            print('The list of zones on page ' + zones_url + ' is not sorted')
+        #возвращение на страницу Geo Zones и новый поиск всех строк таблицы
+        driver.get("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones")
+        rows = driver.find_elements_by_css_selector("table.dataTable tr.row")
