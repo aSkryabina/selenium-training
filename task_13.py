ba57b7414b47d89e2ca1c4c9f621ddf2ad8630db
aSkryabina <aSkryabina@users.noreply.github.com>
2018-08-08 16:49:54 +0300

    Добавление и удаление товаров из корзины (Задание 13)

--- task_13.py
+++ task_13.py
@@ -1,11 +1,9 @@
 import pytest
 from selenium import webdriver
-import random
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
-import os
 
 
 @pytest.fixture
@@ -15,7 +13,7 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
-#проверка наличия элемента
+# проверка наличия элемента
 def find_element(driver,locator):
     try:
         driver.find_element_by_css_selector(locator)
@@ -28,7 +26,7 @@ def add_item(driver):
     driver.get("http://localhost/litecart/")
     # проверяем текущее количество товаров в корзине
     count = int(driver.find_element_by_css_selector("span.quantity").text)
-    # поиск всех товаров и переход в первый найденнй товар
+    # поиск всех товаров и переход в первый найденный товар
     items = driver.find_elements_by_xpath("//li[contains(@class,'product')]")
     items[0].click()
     # если у товара есть обязательный для заполнения выпадающий список, заполняем его
@@ -37,18 +35,21 @@ def add_item(driver):
             "Medium +$2.50")
     # добавляем товар в корзину
     driver.find_element_by_xpath("//button[@name='add_cart_product']").click()
-    return count
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "span.quantity"), str(count+1)))
 
+# нахождение списка товаров в корзине
+def items(driver):
+    driver.get("http://localhost/litecart/en/checkout")
+    list = driver.find_elements_by_css_selector("td.item")
+    return list
 
 # удаление товара из корзины
 def delete_item(driver):
-    # переходим в корзину
-    driver.get("http://localhost/litecart/en/checkout")
-    # находим количество товаров в таблице
-    rows = driver.find_elements_by_css_selector("td.item")
-    count = len(rows)
+    rows = items(driver)
+    count = len(items(driver))
     row = rows[count-1]
-    # если товаров больше одного, то выбираем один из товаров
+    # если разных товаров больше одного, то выбираем один из товаров
     if find_element(driver,"li.shortcut a"):
         driver.find_element_by_css_selector("li.shortcut a").click()
     # удаляем товар
@@ -56,15 +57,15 @@ def delete_item(driver):
     # дожидаемся обновления таблицы
     wait = WebDriverWait(driver, 10)
     wait.until(EC.staleness_of(row))
+    return count
 
 
 def test_trash(driver):
-    items_count = 3
-    for i in range(items_count):
-        count = add_item(driver) + 1
-        wait = WebDriverWait(driver, 10)
-        wait.until(EC.text_to_be_present_in_element((By.CSS_SELECTOR,"span.quantity"), str(count)))
-    for i in range(items_count):
+    for i in range(3):
+        add_item(driver)
+    rows = items(driver)
+    count = len(rows)
+    for i in range(count):
         delete_item(driver)
 
 
