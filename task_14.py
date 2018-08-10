3b27d34914fa1b49e5b214f1788dae9fd7493eed
aSkryabina <aSkryabina@users.noreply.github.com>
2018-08-10 12:23:57 +0300

    Поиск идентификатора открытой страницы

--- task_14.py
+++ task_14.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 import random
 from selenium.webdriver.support.ui import Select
 import os
@@ -13,24 +15,39 @@ def driver(request):
     return wd
 
 
-def test_link(driver):
-    #авторизация в админке
+def test_windows(driver):
+
+    def there_is_window_other_than(old_windows):
+        new_windows = driver.window_handles
+        count = len(old_windows)
+        for i in range(count):
+            new_windows.remove(old_windows[i])
+        new = new_windows[0]
+        return new
+
+    # авторизация в админке
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("remember_me").click()
     driver.find_element_by_name("login").click()
 
+    # страница countries
     driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
+    # поиск внешних ссылок
     driver.find_element_by_xpath("//a[@title='Edit']").click()
     form = driver.find_element_by_xpath("//form")
     links = form.find_elements_by_css_selector("a:not(#address-format-hint)")
     count = len(links)
+    # переход по каждой ссылке из списка
     for i in range(count):
+        main_window = driver.current_window_handle
+        old_windows = driver.window_handles
         links[i].click()
-        driver.get("http://localhost/litecart/admin/?app=countries&doc=countries")
-        driver.find_element_by_xpath("//a[@title='Edit']").click()
-        form = driver.find_element_by_xpath("//form")
-        links = form.find_elements_by_css_selector("a:not(#address-format-hint)")
+        wait = WebDriverWait(driver, 10)
+        new_window = wait.until(there_is_window_other_than(old_windows))
+        driver.switch_to.window(new_window)
+        driver.close()
+        driver.switch_to_window(main_window)
 
 
