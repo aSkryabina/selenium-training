22dc95be1c55c81618e0cd5fa890a271795f6f12
aSkryabina <aSkryabina@users.noreply.github.com>
2018-08-10 14:04:55 +0300

    Переход по ссылкам

--- task_14.py
+++ task_14.py
@@ -2,9 +2,6 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-import random
-from selenium.webdriver.support.ui import Select
-import os
 
 
 @pytest.fixture
@@ -16,15 +13,6 @@ def driver(request):
 
 
 def test_windows(driver):
-
-    def there_is_window_other_than(old_windows):
-        new_windows = driver.window_handles
-        count = len(old_windows)
-        for i in range(count):
-            new_windows.remove(old_windows[i])
-        new = new_windows[0]
-        return new
-
     # авторизация в админке
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
@@ -39,14 +27,16 @@ def test_windows(driver):
     form = driver.find_element_by_xpath("//form")
     links = form.find_elements_by_css_selector("a:not(#address-format-hint)")
     count = len(links)
-    # переход по каждой ссылке из списка
+    # переход по каждой ссылке из списка c с ожиднием открытия нового окна
     for i in range(count):
         main_window = driver.current_window_handle
         old_windows = driver.window_handles
         links[i].click()
         wait = WebDriverWait(driver, 10)
-        new_window = wait.until(there_is_window_other_than(old_windows))
-        driver.switch_to.window(new_window)
+        wait.until(EC.new_window_is_opened(old_windows))
+        new_window = [i for i in driver.window_handles if i not in old_windows]
+        driver.switch_to.window(new_window[0])
+        print("Page is opened: " + driver.current_url)
         driver.close()
         driver.switch_to_window(main_window)
 
