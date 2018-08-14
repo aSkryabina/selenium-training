dca26809d24244402c6084df091bca70ec04d3a1
aSkryabina <aSkryabina@users.noreply.github.com>
2018-08-14 19:07:39 +0300

    Переход по всем товарам в подкатегории

---
+++ task_17.py
@@ -0,0 +1,42 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome()
+    wd.implicitly_wait(5)
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_windows(driver):
+    # авторизация в админке
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name("username").send_keys("admin")
+    driver.find_element_by_name("password").send_keys("admin")
+    driver.find_element_by_name("remember_me").click()
+    driver.find_element_by_name("login").click()
+
+    driver.find_element_by_xpath("//td[@id='sidebar']//span[contains(text(),'Catalog')]").click()
+    driver.find_element_by_xpath("//table[@class='dataTable']//a[contains(text(),'Rubber Ducks')]").click()
+    items = driver.find_elements_by_xpath("//a[contains(.,'Duck') and not(contains(.,'Ducks'))]")
+    for item in items:
+        main_window = driver.current_window_handle
+        old_windows = driver.window_handles
+        href = item.get_attribute("href")
+        driver.execute_script("window.open(\'%s\')" % href)
+        wait = WebDriverWait(driver, 10)
+        wait.until(EC.new_window_is_opened(old_windows))
+        new_window = [i for i in driver.window_handles if i not in old_windows]
+        driver.switch_to.window(new_window[0])
+        driver.close()
+        driver.switch_to_window(main_window)
+
+
+
+
+
